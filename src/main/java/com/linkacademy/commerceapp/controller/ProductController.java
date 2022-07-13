cbab323d002d230347118c80785457d41d5446d4
Parvu Eduard <parvueduard@gmail.com>
2022-07-13 09:48:37 +0300

    - some endpoint tests

---
+++ src/main/java/com/linkacademy/commerceapp/controller/ProductController.java
@@ -0,0 +1,44 @@
+package com.linkacademy.commerceapp.controller;
+
+import com.linkacademy.commerceapp.domain.entity.Product;
+import com.linkacademy.commerceapp.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("commerce/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> findById(@PathVariable("id") UUID id) {
+        Optional<Product> searchedProductOpt = productService.findById(id);
+        return searchedProductOpt
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> searchedProducts = productService.findAll();
+
+        if (searchedProducts == null || searchedProducts.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(searchedProducts);
+    }
+}
---
+++ src/main/java/com/linkacademy/commerceapp/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.linkacademy.commerceapp.service;
+
+import com.linkacademy.commerceapp.domain.entity.Product;
+import com.linkacademy.commerceapp.domain.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Product> findById(UUID id) {
+        return productRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> findAll() {
+        return StreamSupport
+                .stream(productRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+}
